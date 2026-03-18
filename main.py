aa619bafefcaedf2778b81d7c517e4475b620410
timothy-novak21 <timothy.novak21@gmail.com>
2026-03-18 17:34:35 -0500

    updated main.py and simulation.py for M2
    main.py:
    - restructured to add flags to run each part of milestone 1 and 2
    - tokamak setup block added
    - all milestone 2 run blocks added
    - hardcoded Rm array from milestone 1 moved inside respective block
    
    simulation.py:
    - renamed sim_single to sim_single_mirror
    - renamed sim_pitch_sweep to sim_mirror_sweep
    - added sim_single_tokamak, sim_toroid_poloid_comp, sim_tokamak_pitch_sweep, sim_tokamak_q_sweep
    - added Tokamak and check_drift_rate imports
    - updated plotting imports

--- main.py
+++ main.py
@@ -2,37 +2,95 @@ import numpy as np
 from collections import namedtuple
 
 from fields.magnetic_mirror import Mirror
-from simulation import sim_single, sim_rm_sweep
-from analysis.plotting import plot_single
+from fields.tokamak import Tokamak
+from simulation import sim_single_mirror, sim_rm_sweep, sim_single_tokamak, sim_tokamak_pitch_sweep, sim_toroid_poloid_comp, sim_tokamak_q_sweep
+from analysis.plotting import plot_single, plot_single_tokamak, plot_colormap
 
+
+# Milestone 1
+RUN_MIRROR_SWEEP = False
+RUN_MIRROR_SINGLE = False
+
+# Milestone 2
+RUN_TOROIDAL_SWEEP = False
+RUN_TOROIDAL_SINGLE = False
+RUN_POLOIDAL_SINGLE = False
+RUN_TOROIDAL_POLOIDAL_COMP = False
+RUN_Q_SAFETY_SWEEP = False
+RUN_TOKAMAK_COLOR = False
 
 # Constants: q = proton charge [C], m = proton mass [kg]
 PhysicalConstants = namedtuple("PhysicalConstants", ["q","m"])
 CONST = PhysicalConstants(q=1.602e-19, m=1.673e-27)
 
 # Define mirror ratio array and max B field
-Rm = [1.5, 2, 3, 5, 10] # array of mirror ratios [-]
 Bmax = 0.5 # B field strength at mirror [T]
 
 # Define and print initial particle speed
 KE_eV = 5e3 # physically relevant kinetic energy for a proton at fusion conditions [eV]
 v0 = (CONST.q * KE_eV / (0.5 * CONST.m))**0.5 # solve for particle velocity based on desired KE [m/s]
-print(f"Initial particle kinetic energy: {KE_eV/1e3:.1f} keV, Initial particle velocity: {v0/1e6:.2f}e6 m/s")
+# print(f"Initial particle kinetic energy: {KE_eV/1e3:.1f} keV, Initial particle velocity: {v0/1e6:.2f}e6 m/s")
 
-N = 500 # number of pitch angles to sweep [-]
 
-RUN_SWEEP = True
-RUN_SINGLE = False
-
-if RUN_SWEEP:
-    # Run full pitch sweep for all Rm
+if RUN_MIRROR_SWEEP:
+    # Run full mirror pitch sweep for all Rm
+    Rm = [1.5, 2, 3, 5, 10] # array of mirror ratios [-]
+    N = 500 # number of pitch angles to sweep for mirror [-]
     sim_rm_sweep(Rm,Bmax,v0,N,CONST)
 
-if RUN_SINGLE:
-    # Set up and run single sim
+if RUN_MIRROR_SINGLE:
+    # Set up and run single mirror sim
     length = 1 # half length of mirror [m]
     Rm_single = 2 # mirror ratio for single sim [-]
     mirror = Mirror(length,Rm_single,Bmax) # create mirror for single sim
     theta = np.radians(mirror.theta_c + 5) # run at theta slightly higher than critical pitch [rad]
-    state = sim_single(mirror,theta,v0,"single",CONST)
+    mode = "single"
+    state = sim_single_mirror(mirror,theta,v0,mode,CONST)
     plot_single(state,mirror,theta) # plot single
+
+
+# Set up toroidal field
+R0 = 3 # toroidal field major radius [m]
+r0 = 0.9 # toroidal field minor radius [m]
+B0 = Bmax # field strength along toroid centerline [T]
+toroid = Tokamak(R0,r0,B0) # initialize tokamak object with pure toroidal field
+q_safety = 2.0 # safety factor for poloidal field strength [-]
+poloid = Tokamak(R0,r0,B0,q_safety) # initialize tokamak object with poloidal correction
+
+if RUN_TOROIDAL_SWEEP:
+    # Run toroidal pitch sweep
+    N = 101 # number of pitch angles to sweep for toroidal [-]
+    sim_tokamak_pitch_sweep(toroid,v0,N,CONST)
+
+if RUN_TOROIDAL_SINGLE:
+    # Set up and run single sim with pure toroidal field
+    theta = np.radians(20) # pitch angle [rad]
+    mode = "single"
+    cycles = 1
+    state = sim_single_tokamak(toroid,theta,v0,mode,cycles,CONST)
+    plot_single_tokamak(state,toroid,theta)
+
+if RUN_POLOIDAL_SINGLE:
+    # Set up and run single sim with poloidal correction
+    theta = np.radians(20) # pitch angle [rad]
+    mode = "single"
+    cycles = 1
+    state = sim_single_tokamak(poloid,theta,v0,mode,cycles,CONST)
+    plot_single_tokamak(state,poloid,theta)
+
+if RUN_TOROIDAL_POLOIDAL_COMP:
+    theta = np.radians(20) # pitch angle [rad]
+    sim_toroid_poloid_comp(toroid,poloid,v0,theta,CONST)
+
+if RUN_Q_SAFETY_SWEEP:
+    q_list = [1, 1.5, 2, 3, 4, 5, 6, 7, 8, 9, 10] # list of safety factors [-]
+    theta = np.radians(20) # pitch angle [rad]
+    sim_tokamak_q_sweep(q_list,R0,r0,B0,v0,theta,CONST)
+
+if RUN_TOKAMAK_COLOR:
+    # Set up and run tokamak sim with 3d color plot
+    theta = np.radians(20) # pitch angle [rad]
+    mode = "single"
+    cycles = 1
+    state = sim_single_tokamak(poloid,theta,v0,mode,cycles,CONST)
+    plot_colormap(state,poloid,theta)
--- simulation.py
+++ simulation.py
@@ -2,13 +2,15 @@ import numpy as np
 import scipy.integrate as integrate
 
 from fields.magnetic_mirror import Mirror
+from fields.tokamak import Tokamak
 from physics.lorentz import accel
-from analysis.plotting import plot_pitch_sweep, plot_crit_curve
+from analysis.plotting import plot_pitch_sweep, plot_crit_curve, plot_drift, plot_toroid_poloid_comp, plot_q_sweep
 from physics.adiabaticity import invariance_check
 from physics.mu_conservation import check_mu_conservation
+from physics.drift_rate import check_drift_rate
 
 
-def sim_single(mirror,theta,v0,mode,CONST):
+def sim_single_mirror(mirror,theta,v0,mode,CONST):
     """
     Run a single particle magnetic mirror simulation
 
@@ -41,7 +43,7 @@ def sim_single(mirror,theta,v0,mode,CONST):
     v0_perp = v0 * np.sin(theta) # initial particle velocity perpendicular to B field [m/s]
     v0_par = v0 * np.cos(theta) # initial particle velocity parallel to B field [m/s]
 
-    # Package inital state vector
+    # Package initial state vector
     state0 = [0.0, 0.0, 0.0, # initial position [m]
               v0_perp, 0.0, v0_par] # initial velocity [m/s]
     
@@ -97,7 +99,7 @@ def sim_single(mirror,theta,v0,mode,CONST):
     return state
 
 
-def sim_pitch_sweep(mirror,v0,N,CONST):
+def sim_mirror_sweep(mirror,v0,N,CONST):
     """
     Run a series of simulations sweeping through a collection of pitch angles between 1 and 89 degrees
     Calculate and return a simulated critical pitch angle
@@ -133,7 +135,7 @@ def sim_pitch_sweep(mirror,v0,N,CONST):
     mode = "sweep"
     for i in range(0,len(theta)):
         print(f"Running simulation {i+1}/{N}: theta = {np.degrees(theta[i]):.1f} deg")
-        state = sim_single(mirror,theta[i],v0,mode,CONST)
+        state = sim_single_mirror(mirror,theta[i],v0,mode,CONST)
         esc[i] = len(state.t_events[0]) > 0 # t_events[0] == 1 when particle escapes mirror
         
     # Calc simulated critical pitch angle to compare to analytical
@@ -195,12 +197,12 @@ def sim_rm_sweep(Rm,Bmax,v0,N,CONST):
     err_theta_c_sim = np.zeros(len(mirror_list))
     for j in range(0,len(mirror_list)):
         print(f"Running sweep {j + 1:.0f}/{len(mirror_list):.0f}")
-        [theta_c_sim[j],err_theta_c_sim[j]] = sim_pitch_sweep(mirror_list[j],v0,N,CONST) # find simulated critical pitch angle for current mirror/Rm [deg]
+        [theta_c_sim[j],err_theta_c_sim[j]] = sim_mirror_sweep(mirror_list[j],v0,N,CONST) # find simulated critical pitch angle for current mirror/Rm [deg]
 
     # After sweep is completed, run single sim at critical pitch angle to check mu drift
     for k in range(0,len(mirror_list)):
         mode = "mu"
-        state_check = sim_single(mirror_list[k],np.radians(mirror_list[k].theta_c + 5),v0,mode,CONST)
+        state_check = sim_single_mirror(mirror_list[k],np.radians(mirror_list[k].theta_c + 5),v0,mode,CONST)
         mu_drift = check_mu_conservation(state_check,mirror_list[k],CONST)
 
         [epsilon,adiabatic] = invariance_check(v0,mirror_list[k],CONST) # adiabaticity condition for given mirror and v0
@@ -210,3 +212,260 @@ def sim_rm_sweep(Rm,Bmax,v0,N,CONST):
 
     # Plot simulated vs analytical critical pitch angles
     plot_crit_curve(Rm,theta_c_sim)
+
+
+def sim_single_tokamak(tokamak,theta,v0,mode,cycles,CONST):
+    """
+    Run a single particle toroidal field simulation
+
+    Parameters
+    ----------
+    tokamak : Tokamak object
+        Magnetic field object belonging to the Tokamak class stored in tokamak.py
+
+    theta : scalar
+        Particle pitch angle in radians
+
+    v0 : scalar
+        Initial particle speed in meters per second
+
+    mode : string
+        "sweep" | "single" | "banana"
+        Controls integration time and termination events
+        sweep - t_end determined from estimated drift, no termination events
+        single - termiantes after specified number of poloidal cycles through transit event
+        banana - fixed t_end based on bounce period, no termination events
+
+    cycles : scalar
+        Number of poloidal cycles to integrate over, unitless
+
+    CONST : named tuple, shape (2,)
+        Named tuple containing the physical constants q (proton charge) in Coulombs and m (proton mass) in kilograms
+        
+    Returns
+    -------
+    state : OdeResult
+        Solution object from solve_ivp
+    """
+        
+    # Initial conditions
+    v0_perp = v0 * np.sin(theta) # initial particle velocity perpendicular to B field [m/s]
+    v0_par = v0 * np.cos(theta) # initial particle velocity parallel to B field [m/s]
+
+    # Package inital state vector
+    if mode == "banana":
+        state0 = [tokamak.R0 + 0.3, 0.0, 0.0, # start slightly outboard of magnetic axis [m]
+                  0.0, 0.0, v0] # all velocity perpendicular to B [m/s]
+    else:
+        state0 = [tokamak.R0, 0.0, 0.0, # initial position [m]
+                  v0_perp, v0_par, 0.0] # initial velocity [m/s]
+    
+    # Integration time conditions computed based on cyclotron period and estimated drift speed
+    omega_c = CONST.q * tokamak.B0 / CONST.m # cyclotron frequency [1/s]
+    T_cyclotron = 2 * np.pi / omega_c # cyclotron period [s]
+    max_step = T_cyclotron / 200 # 200 steps per gyration for max timestep [s]
+
+    r_L = v0_perp / omega_c # Larmor radius [m]
+    est_gradB = CONST.m * v0_perp**2 / (2 * CONST.q * tokamak.B0 * tokamak.R0) * (1 + r_L**2 / tokamak.R0**2) # estimated grad B drift [m/s]
+    est_curvature = CONST.m * v0_par**2 / (CONST.q * tokamak.B0 * tokamak.R0) # estimated curvature drift [m/s]
+    est_drift = est_gradB + est_curvature # total esimated drift [m/s]
+
+    circ = 2 * np.pi * tokamak.R0 # tokamak circumference [m]
+    T_toroidal = circ / max(abs(v0_par), 1e-6) # estimate toroidal period [s]
+
+    if tokamak.q_safety is not None:
+        T_poloidal = tokamak.q_safety * T_toroidal # estimate poloidal period [s]
+    else: 
+        T_poloidal = T_toroidal # if pure toroidal field there is no poloidal period, set as toroidal period
+
+    # Define event to terminate integration after particle makes specific number of poloidal cycles
+    def transit(t,state,tokamak,CONST):
+        if t < 0.90 * cycles * T_poloidal: # don't allow event to trigger until 90% of predicted time passes
+            return 1
+        eps = 1e-10 # small offset to subtract from initial y position so that event doesn't trigger at start of integration
+        return state[1] - (state0[1] - eps) # full transit is when y position passes intial y position
+    
+    transit.terminal = True # stop integration if particle completes a full transit
+    transit.direction = 1 # stop integration when particle crosses initial x going positive
+
+    if mode == "sweep":
+        # adapative t_end runs simulation until drift covers 20 Larmor radii, with floor of 200 gyrations to ensure z drift dominates noise from individual gyrations
+        t_end = max(20 * r_L / est_drift, 200 * T_cyclotron) # integration end time [s]
+        t_span = [0,t_end] # integration time bounds [s]
+
+        state = integrate.solve_ivp(accel, t_span, state0,
+                                    method="RK45", max_step=max_step,
+                                    rtol=1e-10, atol=1e-12,
+                                    args=(tokamak,CONST))
+    elif mode == "single":
+        # 1.5x multiplier adds extra margin to make sure transit event is what ends integration
+        t_end = 1.5* cycles * T_poloidal # integration end time [s]
+        t_span = [0,t_end] # integration time bounds [s]
+
+        state = integrate.solve_ivp(accel, t_span, state0,
+                                    method="RK45", max_step=max_step,
+                                    rtol=1e-10, atol=1e-12,
+                                    args=(tokamak,CONST),
+                                    events=[transit])
+    elif mode == "banana":
+        # Not currently implemented -- for use in milestone 3
+        # Estimate bounce period from minor radius transit time
+        # Run for 100 bounce periods to fill banana shape in RZ plot
+        T_bounce_est = 4 * tokamak.r0 / v0 # estimate bounce period [s]
+        t_end = 100 * T_bounce_est # integration end time [s]
+        t_span = [0,t_end] # integration time bounds [s]
+
+        state = integrate.solve_ivp(accel, t_span, state0,
+                                    method="RK45", max_step=max_step,
+                                    rtol=1e-10, atol=1e-12,
+                                    args=(tokamak,CONST))
+    
+    return state
+
+
+def sim_toroid_poloid_comp(toroid,poloid,v0,theta,CONST):
+    """
+    Run a single particle simulation for a pure toroidal field and combined field to evaluate drift suppression
+
+    Parameters
+    ----------
+    toroid : Tokamak object
+        Magnetic field object for the pure toroidal field
+
+    poloid : Tokamak object
+        Magnetic field object for the combined toroidal and poloidal field
+
+    v0 : scalar
+        Initial particle speed in meters per second
+
+    theta : scalar
+        Particle pitch angle in radians
+
+    CONST : named tuple, shape (2,)
+        Named tuple containing the physical constants q (proton charge) in Coulombs and m (proton mass) in kilograms
+        
+    Returns
+    -------
+    none
+    """
+
+    # Initial conditions
+    v0_perp = v0 * np.sin(theta) # initial particle velocity perpendicular to B field [m/s]
+    v0_par = v0 * np.cos(theta) # initial particle velocity parallel to B field [m/s]
+
+    state0 = [poloid.R0, 0.0, 0.0, # initial position [m]
+              v0_perp, v0_par, 0.0] # initial velocity [m/s]
+    
+    # Integration time conditions computed based on particle velocity and mirror length
+    omega_c = CONST.q * poloid.B0 / CONST.m # cyclotron frequency [1/s]
+    T_cyclotron = 2 * np.pi / omega_c # cyclotron period [s]
+    max_step = T_cyclotron / 200 # 200 steps per gyration for max timestep [s]
+    
+    circ = 2 * np.pi * poloid.R0
+    T_toroidal = circ / max(abs(v0_par), 1e-6)
+        
+    T_poloidal = poloid.q_safety * T_toroidal
+
+    t_end = T_poloidal
+    t_span = [0,t_end]
+
+
+    state_tor = integrate.solve_ivp(accel, t_span, state0,
+                                method="RK45", max_step=max_step,
+                                rtol=1e-10, atol=1e-12,
+                                args=(toroid,CONST))
+    
+    state_pol = integrate.solve_ivp(accel, t_span, state0,
+                                method="RK45", max_step=max_step,
+                                rtol=1e-10, atol=1e-12,
+                                args=(poloid,CONST))
+    
+    plot_toroid_poloid_comp(state_tor,state_pol,poloid,theta)
+    
+
+def sim_tokamak_pitch_sweep(tokamak,v0,N,CONST):
+    """
+    Run simulations sweeping through pitch angles to compare to analytic drift predictions
+
+    Parameters
+    ----------
+    tokamak : Tokamak object
+        Magnetic field object belonging to the Tokamak class stored in tokamak.py
+
+    v0 : scalar
+        Initial particle speed in meters per second
+
+    N : scalar
+        Number of pitch angles to sweep through, unitless
+
+    CONST : named tuple, shape (2,)
+        Named tuple containing the physical constants q (proton charge) in Coulombs and m (proton mass) in kilograms
+        
+    Returns
+    -------
+    none
+    """
+        
+    theta = np.radians(np.linspace(0,90,N)) # theta list to sweep
+
+    # initialize empty arrays for results
+    drift_sim = np.zeros(len(theta))
+    drift_analytic = np.zeros(len(theta))
+    err = np.zeros(len(theta))
+
+    # run sweep
+    for i in range(0,len(theta)):
+        print(f"Running simulation {i+1}/{len(theta)}")
+        mode = "sweep"
+        cycles = 1
+        state = sim_single_tokamak(tokamak,theta[i],v0,mode,cycles,CONST)
+        drift_sim[i], drift_analytic[i], err[i] = check_drift_rate(state,tokamak,v0,theta[i],CONST)
+
+    # plot results
+    plot_drift(drift_sim,theta,err,tokamak,v0,CONST)
+
+
+def sim_tokamak_q_sweep(q_list,R0,r0,B0,v0,theta,CONST):
+    """
+    Run tokamak simulations sweeping through a list of safety factors to validate linear relationship between q and z excursion
+
+    Parameters
+    ----------
+    q_list : array-like, size (N,)
+        List containing the safety factor values to sweep through
+
+    R0 : scalar
+        Major radius of tokamak in meters
+
+    r0 : scalar
+        Minor radius of tokamak in meters
+
+    B0 : scalar
+        Magnetic field strength at tokmaka major radius in Tesla
+
+    v0 : scalar
+        Initial particle speed in meters per second
+
+    theta : scalar
+        Particle pitch angle in radians
+
+    CONST : named tuple, shape (2,)
+        Named tuple containing the physical constants q (proton charge) in Coulombs and m (proton mass) in kilograms
+        
+    Returns
+    -------
+    none
+    """
+    
+    z_ex = np.zeros(len(q_list)) # initialize empty array for excursion
+
+    # run sweep
+    for i in range(0,len(q_list)):
+        tokamak = Tokamak(R0,r0,B0,q_list[i])
+        mode = "single"
+        cycles = 1
+        state = sim_single_tokamak(tokamak,theta,v0,mode,cycles,CONST)
+        z_ex[i] = np.max(state.y[2]) - np.min(state.y[2])
+        
+    # plot results
+    plot_q_sweep(q_list,z_ex,tokamak,v0,theta,CONST)
